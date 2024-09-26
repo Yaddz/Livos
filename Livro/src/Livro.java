47b4e5caf37f87561c3a7e43a25c63644a4f19bf
Pedro <85305216+Yaddz@users.noreply.github.com>
2024-09-26 10:24:23 -0300

    Add code

---
+++ Livro/src/Livro.java
@@ -0,0 +1,17 @@
+public class Livro {
+    public String titulo;
+    public String autor;
+    public double preco;
+
+    public Livro(String titulo, String autor, double preco) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.preco = preco;
+    }
+
+    public void exibirDetalhes(){
+        System.out.println("Título: " + titulo);
+        System.out.println("Autor: " + autor);
+        System.out.println("Preço: R$ " + preco);
+    }
+}
